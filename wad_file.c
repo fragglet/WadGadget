57699afe4c7f29f01a903d4ab292e9ba67be8e02
Simon Howard <fraggle@soulsphere.org>
2022-07-04 23:30:16 -0400

    Change wad_file to use vfile.

--- wad_file.c
+++ wad_file.c
@@ -4,10 +4,11 @@
 #include <string.h>
 #include <assert.h>
 
+#include "vfile.h"
 #include "wad_file.h"
 
 struct wad_file {
-	FILE *fs;
+	VFILE *vfs;
 	struct wad_file_entry *directory;
 	int num_lumps;
 };
@@ -17,25 +18,28 @@ struct wad_file *W_OpenFile(const char *file)
 	struct wad_file *result;
 	struct wad_file_header hdr;
 	FILE *fs;
+	VFILE *vfs;
 
 	fs = fopen(file, "r");
 	if (fs == NULL) {
 		return NULL;
 	}
 
-	assert(fread(&hdr, sizeof(struct wad_file_header), 1, fs) == 1);
+	vfs = vfwrapfile(fs);
+
+	assert(vfread(&hdr, sizeof(struct wad_file_header), 1, vfs) == 1);
 	assert(!strncmp(hdr.id, "IWAD", 4) ||
 	       !strncmp(hdr.id, "PWAD", 4));
-	assert(fseek(fs, hdr.table_offset, SEEK_SET) == 0);
+	assert(vfseek(vfs, hdr.table_offset) == 0);
 
 	result = calloc(1, sizeof(struct wad_file));
 	assert(result != NULL);
-	result->fs = fs;
+	result->vfs = vfs;
 	result->num_lumps = hdr.num_lumps;
 	result->directory = calloc(hdr.num_lumps, sizeof(struct wad_file_entry));
 	assert(result->directory != NULL);
-	assert(fread(result->directory, sizeof(struct wad_file_entry),
-	             hdr.num_lumps, fs) == hdr.num_lumps);
+	assert(vfread(result->directory, sizeof(struct wad_file_entry),
+	              hdr.num_lumps, vfs) == hdr.num_lumps);
 
 	return result;
 }
@@ -52,7 +56,7 @@ unsigned int W_NumLumps(struct wad_file *f)
 
 void W_CloseFile(struct wad_file *f)
 {
-	fclose(f->fs);
+	vfclose(f->vfs);
 	free(f);
 }
 
