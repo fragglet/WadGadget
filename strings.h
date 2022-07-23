1dc340a31ba5ba371b1e4a7d7e1987340afd9dfd
Simon Howard <fraggle@soulsphere.org>
2022-07-23 17:33:20 -0400

    Oops

--- strings.h
+++ strings.h
@@ -7,7 +7,7 @@ int StringConcat(char *dest, const char *src, size_t dest_size);
 int StringHasPrefix(const char *s, const char *prefix);
 int StringHasSuffix(const char *s, const char *suffix);
 const char *StrCaseStr(const char *haystack, const char *needle);
-char *StringReplace(const char *haystack, const char *needle,;
+char *StringReplace(const char *haystack, const char *needle);
 char *StringJoin(const char *sep, const char *s, ...);
 int VStringPrintf(char *buf, size_t buf_len, const char *s, va_list args);
 int StringPrintf(char *buf, size_t buf_len, const char *s, ...);
