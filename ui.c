58254e032dbf0b4256dfca41f82864e2f618b790
Simon Howard <fraggle@soulsphere.org>
2022-07-25 22:31:45 -0400

    Add auto-expanding list of games.
    A cute feature reminiscent of NWT, which listed all the games it supported
    on startup. By NWT 1.4, the list had grown quite long, and yet it was
    before even Final Doom was released. Since we support arbitrary window
    sizes we can automatically adjust the number of games that are shown
    depending on the screen width. There's a nice long list of games that
    use the WAD file format.

--- ui.c
+++ ui.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 #include <curses.h>
 
 #include "colors.h"
@@ -52,14 +53,60 @@ void UI_PrintMultilineString(WINDOW *win, int y, int x, const char *s)
 	}
 }
 
+// What games use the WAD format?
+static const char *games[] = {
+	"Doom", "[Doom II", "[Final Doom", "Heretic", "Hexen", "Strife",
+	"[Chex Quest", "[Freedoom", "[Rise of the Triad", "[HACX",
+	"[Amulets & Armor", "[Duke Nukem 3D", NULL,
+};
+
+#define START_STR "= WadGadget for "
+#define END_STR "\b\b and the rest ="
+
 static void DrawHeaderPane(void *p)
 {
 	struct pane *pane = p;
+	int w, h, x, y;
+	int i, count_extra;
+
+	getmaxyx(pane->window, h, w);
+	h = h;
 
 	wbkgdset(pane->window, COLOR_PAIR(PAIR_HEADER));
 	werase(pane->window);
-	mvwaddstr(pane->window, 0, 1, "= WadGadget for Doom, Heretic, Hexen, "
-	                "Strife, Chex Quest and the rest =");
+	mvwaddstr(pane->window, 0, 1, START_STR);
+
+	count_extra = 0;
+	x = strlen(START_STR) + strlen(END_STR);
+	for (i = 0; games[i] != NULL && x + strlen(games[i]) < w; i++) {
+		if (games[i][0] != '[') {
+			x += strlen(games[i]) + 2;
+		}
+	}
+	for (i = 0; games[i] != NULL && x + strlen(games[i]) < w; i++) {
+		if (games[i][0] == '[') {
+			x += strlen(games[i]) + 1;
+			++count_extra;
+		}
+	}
+	x = 0;
+	for (i = 0; games[i] != NULL; i++) {
+		if (x + strlen(games[i]) > w - strlen(END_STR)) {
+			continue;
+		}
+		if (games[i][0] != '[') {
+			waddstr(pane->window, games[i]);
+			waddstr(pane->window, ", ");
+		} else if (count_extra > 0) {
+			waddstr(pane->window, games[i] + 1);
+			waddstr(pane->window, ", ");
+			--count_extra;
+		}
+		getyx(pane->window, y, x);
+		y = y;
+	}
+
+	waddstr(pane->window, END_STR);
 }
 
 void UI_InitHeaderPane(struct pane *pane, WINDOW *win)
