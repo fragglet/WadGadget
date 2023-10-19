ff365fde70a66c841d215ce49bc8b2748a2555af
Simon Howard <fraggle@soulsphere.org>
2023-10-19 18:19:55 -0400

    Don't perform another search on backspace.
    It doesn't make any sense to reset the position until we type
    something new to search for, and jumping on backspace just provides an
    unnecessary visual distraction.

--- wadgadget.c
+++ wadgadget.c
@@ -192,7 +192,9 @@ static void SearchPaneKeypress(void *pane, int key)
 	struct search_pane *p = pane;
 	// Space key triggers mark, does not go to search input.
 	if (key != ' ' && UI_TextInputKeypress(&p->input, key)) {
-		UI_ListPaneSearch(panes[active_pane], p->input.input);
+		if (key != KEY_BACKSPACE) {
+			UI_ListPaneSearch(panes[active_pane], p->input.input);
+		}
 	} else {
 		HandleKeypress(NULL, key);
 	}
