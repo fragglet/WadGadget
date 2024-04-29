383a84ca3377995c9338366f1031b8bf3c0db127
Simon Howard <fraggle@soulsphere.org>
2024-04-28 23:21:03 -0400

    License boilerplate one line shorter.

--- mus2mid.h
+++ mus2mid.h
@@ -3,12 +3,11 @@
 // Copyright(C) 2005-2024 Simon Howard
 // Copyright(C) 2006 Ben Ryves 2006
 //
-// This program is free software; you can redistribute it and/or modify it
-// under the terms of the GNU General Public License as published by the
-// Free Software Foundation; either version 2 of the License, or (at your
-// option) any later version. This program is distributed in the hope that
-// it will be useful, but WITHOUT ANY WARRANTY; without even the implied
-// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// You can redistribute and/or modify this program under the terms of
+// the GNU General Public License version 2 as published by the Free
+// Software Foundation, or any later version. This program is
+// distributed WITHOUT ANY WARRANTY; without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 //
 //
 // mus2mid.h - Ben Ryves 2006 - http://benryves.com - benryves@benryves.com
