c604723777a79183ac09f492da54cba5ee07c8d1
Simon Howard <fraggle@soulsphere.org>
2024-05-12 21:10:45 -0400

    Add endianness conversion functions.
    First step towards fixing the endianness bugs that I've ignored until
    now out of laziness.

--- common.h
+++ common.h
@@ -8,6 +8,7 @@
 // of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 //
 
+#include <stdint.h>
 #include <assert.h>
 
 #define SHIFT_KEY_F(n) KEY_F(n + 12)
@@ -36,4 +37,33 @@ static inline void *check_allocation_result(void *x)
 
 #define checked_strdup(s) \
     check_allocation_result(strdup(s))
+
+// Endianness conversions.
+static inline void SwapLE32(void *i)
+{
+	uint8_t *b = i;
+	uint32_t *i32 = i;
+	*i32 = (b[3] << 24) | (b[2] << 16) | (b[1] << 8) | b[0];
+}
+
+static inline void SwapBE32(void *i)
+{
+	uint8_t *b = i;
+	uint32_t *i32 = i;
+	*i32 = (b[0] << 24) | (b[1] << 16) | (b[2] << 8) | b[3];
+}
+
+static inline void SwapLE16(void *i)
+{
+	uint8_t *b = i;
+	uint16_t *i16 = i;
+	*i16 = (b[1] << 8) | b[0];
+}
+
+static inline void SwapBE16(void *i)
+{
+	uint8_t *b = i;
+	uint16_t *i16 = i;
+	*i16 = (b[0] << 8) | b[1];
+}
 
