0ac875c4409e0db39e1f5dc714964de866051271
Simon Howard <fraggle@soulsphere.org>
2024-05-26 01:03:09 -0400

    Add missing copyright headers.

--- sixel_display.h
+++ sixel_display.h
@@ -1,3 +1,12 @@
+//
+// Copyright(C) 2024 Simon Howard
+//
+// You can redistribute and/or modify this program under the terms of
+// the GNU General Public License version 2 as published by the Free
+// Software Foundation, or any later version. This program is
+// distributed WITHOUT ANY WARRANTY; without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//
 
 bool SIXEL_CheckSupported(void);
 void SIXEL_ClearAndPrint(const char *msg, ...);
