fragglet/WadGadget

commit 53bbb61d98cb3974e0352da794760a45413cc19e

Replace `TX_GetDirPnames` with `TX_DirGetBundle`.
We no longer need a callback function to retrieve this since we use
the same bundle type for both directory types.

src/textures/actions.c
@@ -260,7 +260,7 @@ const struct action export_texture_config = {
 static void PerformNewPname(struct directory_pane *active_pane,
                             struct directory_pane *other_pane)
 {
-	struct pnames *pn = TX_GetDirPnames(active_pane->dir);
+	struct texture_bundle *b = TX_DirGetBundle(active_pane->dir);
 	int idx;
 	char *name;
 
@@ -274,7 +274,7 @@ static void PerformNewPname(struct directory_pane *active_pane,
 		return;
 	}
 
-	if (TX_GetPnameIndex(pn, name) >= 0) {
+	if (TX_GetPnameIndex(b->pn, name) >= 0) {
 		UI_DirectoryPaneSelectByName(active_pane, name);
 		UI_MessageBox("'%s' is already in the list.");
 		free(name);
@@ -286,7 +286,7 @@ static void PerformNewPname(struct directory_pane *active_pane,
 	// That's not to say that we don't allow it to be subsequently moved
 	// into a different position, but just adding a pname always does
 	// something safe.
-	idx = TX_AppendPname(pn, name);
+	idx = TX_AppendPname(b->pn, name);
 	VFS_CommitChanges(active_pane->dir, "creation of pname '%s'", name);
 	VFS_Refresh(active_pane->dir);
 	UI_DirectoryPaneSelectEntry(active_pane,
@@ -333,7 +333,7 @@ static void PerformCopyPnames(struct directory_pane *active_pane,
 	struct file_set copied = EMPTY_FILE_SET, unused = EMPTY_FILE_SET;
 	struct directory *from_dir = active_pane->dir,
 	                 *to_dir = other_pane->dir;
-	struct pnames *pn = TX_GetDirPnames(other_pane->dir);
+	struct texture_bundle *b = TX_DirGetBundle(active_pane->dir);
 	struct directory_entry *ent;
 	int idx;
 
@@ -348,11 +348,11 @@ static void PerformCopyPnames(struct directory_pane *active_pane,
 
 	idx = 0;
 	while ((ent = VFS_IterateSet(from_dir, tagged, &idx)) != NULL) {
-		if (TX_GetPnameIndex(pn, ent->name) >= 0) {
+		if (TX_GetPnameIndex(b->pn, ent->name) >= 0) {
 			VFS_AddToSet(&unused, TX_PnameSerialNo(ent->name));
 			continue;
 		}
-		TX_AppendPname(pn, ent->name);
+		TX_AppendPname(b->pn, ent->name);
 		VFS_AddToSet(&copied, TX_PnameSerialNo(ent->name));
 	}
 

src/textures/internal.h
@@ -9,7 +9,6 @@
 //
 
 struct lump_dir_funcs {
-	struct pnames *(*get_pnames)(void *dir);
 	bool (*load)(void *dir, struct directory *wad_dir,
 	             struct directory_entry *ent);
 	bool (*save)(void *dir, struct directory *wad_dir,

src/textures/lump_dir.c
@@ -99,11 +99,11 @@ bool TX_DirSave(struct directory *_dir)
 	return result;
 }
 
-struct pnames *TX_GetDirPnames(struct directory *_dir)
+struct texture_bundle *TX_DirGetBundle(struct directory *_dir)
 {
 	struct lump_dir *dir = (struct lump_dir *) _dir;
 
-	return dir->lump_dir_funcs->get_pnames(dir);
+	return &dir->b;
 }
 
 VFILE *TX_DirFormatConfig(struct directory *_dir, struct file_set *subset)

src/textures/pnames_dir.c
@@ -168,13 +168,6 @@ static const struct directory_funcs pnames_dir_funcs = {
 	PnamesDirFree,
 };
 
-struct pnames *PnamesDirGetPnames(void *_dir)
-{
-	struct pnames_dir *dir = _dir;
-	assert(dir->dir.dir.directory_funcs == &pnames_dir_funcs);
-	return PNAMES(dir);
-}
-
 static bool PnamesDirLoad(void *_dir, struct directory *wad_dir,
                           struct directory_entry *ent)
 {
@@ -249,7 +242,6 @@ static VFILE *PnamesDirFormatConfig(void *_dir, struct file_set *selected)
 }
 
 static const struct lump_dir_funcs pnames_lump_dir_funcs = {
-	PnamesDirGetPnames,
 	PnamesDirLoad,
 	PnamesDirSave,
 	PnamesDirFormatConfig,

src/textures/texture_dir.c
@@ -201,14 +201,6 @@ static struct pnames *LoadPnames(struct texture_dir *dir)
 	return pn;
 }
 
-static struct pnames *TextureDirGetPnames(void *_dir)
-{
-	struct texture_dir *dir = _dir;
-
-	assert(dir->dir.dir.directory_funcs == &texture_dir_funcs);
-	return PNAMES(dir);
-}
-
 static bool TextureDirLoad(void *_dir, struct directory *wad_dir,
                            struct directory_entry *ent)
 {
@@ -315,7 +307,6 @@ static VFILE *TextureDirFormatConfig(void *_dir, struct file_set *selected)
 }
 
 static const struct lump_dir_funcs texture_lump_dir_funcs = {
-	TextureDirGetPnames,
 	TextureDirLoad,
 	TextureDirSave,
 	TextureDirFormatConfig,

src/textures/textures.h
@@ -82,7 +82,7 @@ bool TX_DirReload(struct directory *_dir);
 struct textures *TX_TextureList(struct directory *_dir);
 struct directory *TX_DirGetParent(struct directory *_dir,
                                   struct directory_entry **ent);
-struct pnames *TX_GetDirPnames(struct directory *_dir);
+struct texture_bundle *TX_DirGetBundle(struct directory *_dir);
 struct directory *TX_OpenPnamesDir(struct directory *parent,
                                    struct directory_entry *ent);
 VFILE *TX_DirFormatConfig(struct directory *_dir, struct file_set *subset);

commit 5960f3ab41cf223db46597fd4605e0ce07f0ae88

Fix bundle call to use the correct pane.
This got broken in commit 53bbb61d98cb3974e.

src/textures/actions.c
@@ -378,7 +378,7 @@ static void PerformCopyPnames(struct directory_pane *active_pane,
 	struct file_set copied = EMPTY_FILE_SET, unused = EMPTY_FILE_SET;
 	struct directory *from_dir = active_pane->dir,
 	                 *to_dir = other_pane->dir;
-	struct texture_bundle *b = TX_DirGetBundle(active_pane->dir);
+	struct texture_bundle *b = TX_DirGetBundle(to_dir);
 	struct directory_entry *ent;
 	int idx;
 