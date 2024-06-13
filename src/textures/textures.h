530d7ed6738912a5948005262dae1861af0011a3
Simon Howard <fraggle@soulsphere.org>
2024-06-12 21:05:59 -0400

    Add missing extern definition for new action.

--- src/textures/textures.h
+++ src/textures/textures.h
@@ -93,6 +93,7 @@ extern const struct action new_texture_action;
 extern const struct action edit_textures_action;
 extern const struct action edit_pnames_action;
 extern const struct action dup_texture_action;
+extern const struct action import_texture_config;
 extern const struct action export_texture_config;
 extern const struct action new_pname_action;
 extern const struct action copy_pnames_action;
