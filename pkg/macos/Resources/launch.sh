c58d17952cbbdbe7569a0e130b8bd5f1e25d0536
Simon Howard <fraggle@soulsphere.org>
2024-06-15 15:18:57 -0400

    Workaround macOS "unknown developer" bug.
    It is possible to bypass developer signing restrictions by opening
    the app manually within the Finder, but the program needs to run the
    executable directly. Since we launch it through the terminal, we need
    to at least invoke it directly once first.

--- pkg/macos/Resources/launch.sh
+++ pkg/macos/Resources/launch.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
 path=$(dirname "$0")
+
+# If this script is running, the user has granted permission for this
+# app to run. We need to pass that permission to the main executable
+# before we launch it in the terminal, otherwise the same permission
+# error will occur again.
+"$path/wadgadget" --version
+
 open "$path/wadgadget"
