1d735a05f27f1fc3475b83c638398e2f32b4b02b
Simon Howard <fraggle@soulsphere.org>
2024-07-02 13:35:28 -0400

    Add action to follow link

--- src/pager/help.c
+++ src/pager/help.c
@@ -24,6 +24,19 @@
 #include "ui/actions_bar.h"
 #include "ui/dialog.h"
 
+static const char *HelpFileContents(const char *filename)
+{
+	int i;
+
+	for (i = 0; help_files[i].filename != NULL; i++) {
+		if (!strcmp(help_files[i].filename, filename)) {
+			return help_files[i].contents;
+		}
+	}
+
+	return NULL;
+}
+
 static bool HaveSyntaxElements(const char *start, const char *el1, ...)
 {
 	va_list args;
@@ -55,10 +68,18 @@ static bool IsLinkStart(const char *p)
 	return HaveSyntaxElements(p, "[", "](", ")", NULL);
 }
 
+static const char *LineHasLink(const char *p)
+{
+	char *q = strstr(p, "[");
+	if (q != NULL && IsLinkStart(q)) {
+		return p;
+	}
+	return NULL;
+}
+
 static bool ScanNextLink(struct help_pager_config *cfg)
 {
 	int win_h = 25;
-	char *p;
 	int lineno = cfg->current_link_line;
 
 	if (current_pager != NULL && current_pager->pane.window != NULL) {
@@ -66,18 +87,19 @@ static bool ScanNextLink(struct help_pager_config *cfg)
 	}
 
 	while (lineno < cfg->pc.num_lines) {
-		p = strstr(cfg->lines[lineno], "[");
-		if (p != NULL && IsLinkStart(p)) {
-			cfg->current_link_line = lineno;
-			if (current_pager != NULL &&
-			    (lineno < current_pager->window_offset
-			  || lineno > current_pager->window_offset + win_h)) {
-				current_pager->window_offset =
-					max(lineno - 5, 0);
-			}
-			return true;
+		if (!LineHasLink(cfg->lines[lineno])) {
+			++lineno;
+			continue;
 		}
-		++lineno;
+
+		// This line has a link.
+		cfg->current_link_line = lineno;
+		if (current_pager != NULL &&
+		    (lineno < current_pager->window_offset
+		  || lineno >= current_pager->window_offset + win_h)) {
+			current_pager->window_offset = max(lineno - 5, 0);
+		}
+		return true;
 	}
 
 	return false;
@@ -103,9 +125,63 @@ static const struct action next_link_action = {
         '\t', 0, "Next Link", "Next Link", PerformNextLink,
 };
 
+static void FreeLines(struct help_pager_config *cfg)
+{
+	int i;
+
+	for (i = 0; i < cfg->pc.num_lines; i++) {
+		free(cfg->lines[i]);
+	}
+	free(cfg->lines);
+}
+
+static bool OpenHelpFile(struct help_pager_config *cfg, const char *filename)
+{
+	const char *contents = HelpFileContents(filename);
+	if (contents == NULL) {
+		UI_MessageBox("Can't find help file '%s'", filename);
+		return false;
+	}
+
+	FreeLines(cfg);
+	cfg->lines = P_PlaintextLines(contents, strlen(contents),
+	                              &cfg->pc.num_lines);
+	cfg->current_link_line = -1;
+	JumpNextLink(cfg);
+
+	return true;
+}
+
+static void PerformFollowLink(void)
+{
+	struct help_pager_config *cfg = current_pager->cfg->user_data;
+	const char *line = cfg->lines[cfg->current_link_line];
+	const char *link_middle = strstr(line, "](");
+	char *filename, *p;
+
+	if (link_middle == NULL) {
+		return;
+	}
+
+	filename = strdup(link_middle + 2);
+	p = strchr(filename, ')');
+	if (p == NULL) {
+		return;
+	}
+
+	*p = '\0';
+	OpenHelpFile(cfg, filename);
+	free(filename);
+}
+
+static const struct action follow_link_action = {
+	'\r', 0, "Open Link", "Open Link", PerformFollowLink,
+};
+
 static const struct action *help_pager_actions[] = {
 	&exit_pager_action,
 	&next_link_action,
+	&follow_link_action,
 	NULL,
 };
 
@@ -166,47 +242,19 @@ static void DrawHelpLine(WINDOW *win, unsigned int lineno, void *user_data)
 
 void P_FreeHelpConfig(struct help_pager_config *cfg)
 {
-	int i;
-
-	for (i = 0; i < cfg->pc.num_lines; i++) {
-		free(cfg->lines[i]);
-	}
-	free(cfg->lines);
-}
-
-static const char *HelpFileContents(const char *filename)
-{
-	int i;
-
-	for (i = 0; help_files[i].filename != NULL; i++) {
-		if (!strcmp(help_files[i].filename, filename)) {
-			return help_files[i].contents;
-		}
-	}
-
-	return NULL;
+	FreeLines(cfg);
 }
 
 bool P_InitHelpConfig(struct help_pager_config *cfg, const char *filename)
 {
-	const char *contents;
-
 	cfg->pc.title = "WadGadget help";
 	cfg->pc.draw_line = DrawHelpLine;
 	cfg->pc.user_data = cfg;
 	cfg->pc.actions = help_pager_actions;
+	cfg->lines = NULL;
+	cfg->pc.num_lines = 0;
 
-	contents = HelpFileContents(filename);
-	if (contents == NULL) {
-		UI_MessageBox("Can't find help file '%s'", filename);
-		return false;
-	}
-
-	cfg->lines = P_PlaintextLines(contents, strlen(contents),
-	                              &cfg->pc.num_lines);
-	cfg->current_link_line = -1;
-	JumpNextLink(cfg);
-	return true;
+	return OpenHelpFile(cfg, filename);
 }
 
 bool P_RunHelpPager(const char *filename)
