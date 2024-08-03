fragglet/WadGadget

commit 8f191dcd2e12ba1abf8cc5069949e5ebcd935755

Add first actions for palette directory.
We add a special view action that is a wrapper around the normal view
action; this allows us to scope the PAL_Inner* calls to the code in the
palette/ directory.

src/browser/browser.c
@@ -33,6 +33,8 @@
 #include "browser/directory_pane.h"
 #include "browser/browser.h"
 
+#include "palette/actions.h"
+
 #define INFO_PANE_WIDTH 30
 
 struct search_pane {
@@ -211,6 +213,7 @@ static const struct action *wad_actions[] = {
 	&sort_entries_action,
 	&hexdump_action,
 	&open_palettes_action,
+	&view_action,
 	NULL,
 };
 
@@ -222,6 +225,7 @@ static const struct action *dir_actions[] = {
 	&mkdir_action,
 	&hexdump_action,
 	&open_palettes_action,
+	&view_action,
 	NULL,
 };
 
@@ -247,6 +251,8 @@ static const struct action *pnm_actions[] = {
 };
 
 static const struct action *pal_actions[] = {
+	&set_default_palette_action,
+	&view_palette_action,
 	NULL,
 };
 
@@ -339,7 +345,6 @@ static const struct action *common_actions[] = {
 	&reload_action,
 	&mark_action,
 	&delete_no_confirm_action,
-	&view_action,
 	&other_pane_action,
 	&help_action,
 	&quit_action,

src/GNUmakefile
@@ -58,6 +58,7 @@ OBJS =                      \
     pager/hexdump.o         \
     pager/pager.o           \
     pager/plaintext.o       \
+    palette/actions.o       \
     palette/doom.o          \
     palette/palette.o       \
     palette/palfs.o         \

src/browser/actions.c
@@ -1022,11 +1022,6 @@ static void PerformView(void)
 	dir = active_pane->dir;
 	ent = B_DirectoryPaneEntry(active_pane);
 
-	if (ent->type == FILE_TYPE_PALETTE) {
-		ent = PAL_InnerEntry(dir, ent);
-		dir = PAL_InnerDir(dir);
-	}
-
 	switch (ent->type) {
 	case FILE_TYPE_DIR:
 	case FILE_TYPE_WAD:

src/palette/actions.c
@@ -1,0 +1,60 @@
+//
+// Copyright(C) 2022-2024 Simon Howard
+//
+// You can redistribute and/or modify this program under the terms of
+// the GNU General Public License version 2 as published by the Free
+// Software Foundation, or any later version. This program is
+// distributed WITHOUT ANY WARRANTY; without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+#include "ui/actions_bar.h"
+#include "browser/actions.h"
+#include "browser/browser.h"
+#include "browser/directory_pane.h"
+#include "view.h"
+
+#include "palette/palette.h"
+#include "palette/palfs.h"
+
+static void PerformViewPalette(void)
+{
+	struct directory_entry *ent = B_DirectoryPaneEntry(active_pane);
+
+	if (ent->type == FILE_TYPE_PALETTE) {
+		OpenDirent(PAL_InnerDir(active_pane->dir),
+		           PAL_InnerEntry(active_pane->dir, ent), false);
+		return;
+	}
+
+	// Otherwise, do the normal "View" action thing:
+	view_action.callback();
+}
+
+const struct action view_palette_action = {
+	'\r', 0,  "View", "View",
+	PerformViewPalette,
+};
+
+static void PerformSetDefault(void)
+{
+	struct directory_entry *ent = B_DirectoryPaneEntry(active_pane);
+
+	if (ent->type != FILE_TYPE_PALETTE) {
+		return;
+	}
+
+	ent = PAL_InnerEntry(active_pane->dir, ent);
+
+	PAL_SetDefaultPointer(ent->name);
+	VFS_Refresh(active_pane->dir);
+}
+
+const struct action set_default_palette_action = {
+	KEY_F(4), 'D',  "SetDef", "Set default",
+	PerformSetDefault,
+};

src/palette/actions.h
@@ -1,0 +1,17 @@
+//
+// Copyright(C) 2022-2024 Simon Howard
+//
+// You can redistribute and/or modify this program under the terms of
+// the GNU General Public License version 2 as published by the Free
+// Software Foundation, or any later version. This program is
+// distributed WITHOUT ANY WARRANTY; without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//
+
+#ifndef PALETTE_ACTIONS_H
+#define PALETTE_ACTIONS_H
+
+extern const struct action view_palette_action;
+extern const struct action set_default_palette_action;
+
+#endif  /* #ifdef PALETTE_ACTIONS_H */

commit 8c40811ad3e9538496eb3dd7c674fcf2592155ed

Fix parent link on pnames/texture dirs.
This was broken in commit 8f191dcd when view_action got moved out of
the common_actions[] array.

src/browser/browser.c
@@ -237,6 +237,7 @@ static const struct action *txt_actions[] = {
 	&dup_texture_action,
 	&undo_action,
 	&redo_action,
+	&view_action,
 	NULL,
 };
 
@@ -247,6 +248,7 @@ static const struct action *pnm_actions[] = {
 	&new_pname_action,
 	&undo_action,
 	&redo_action,
+	&view_action,
 	NULL,
 };
 