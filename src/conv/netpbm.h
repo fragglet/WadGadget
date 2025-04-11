bf310f8c1270912230b42f2ee62b29ac5d542479
Simon Howard <fraggle@soulsphere.org>
2025-04-11 18:09:21 -0400

    Add conversion from other image formats via NetPBM

--- src/conv/netpbm.h
+++ src/conv/netpbm.h
@@ -8,11 +8,14 @@
 // of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 //
 
-#ifndef CONV__PROCESS_H_IMPORTED
-#define CONV__PROCESS_H_IMPORTED
+#ifndef CONV__NETPBM_H_IMPORTED
+#define CONV__NETPBM_H_IMPORTED
+
+#include <stdbool.h>
 
 #include "fs/vfile.h"
 
-VFILE *SpawnSubprocessFilter(VFILE *input, const char **cmd);
+bool NetpbmFileTypeSupported(const char *filename);
+VFILE *NetpbmConvertToPNG(VFILE *input, const char *filename);
 
-#endif /* #ifndef CONV__PROCESS_H_IMPORTED */
+#endif /* #ifndef CONV__NETPBM_H_IMPORTED */
