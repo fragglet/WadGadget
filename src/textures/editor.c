b3a709db945ba48e50c82c5fd2b766c563881da9
Simon Howard <fraggle@soulsphere.org>
2025-04-15 16:23:46 -0400

    Delete resolved TODO comment

--- src/textures/editor.c
+++ src/textures/editor.c
@@ -195,7 +195,6 @@ static void EditorDrawLine(WINDOW *win, unsigned int line, void *user_data)
 	patch = &TX(e)->patches[patch_idx];
 
 	waddstr(win, "         ");
-	// TODO: Real patch name
 	DrawField(win, 3 + patch_idx * 3, "%-8.8s",
 	          e->b->pn->pnames[patch->patch]);
 	DrawField(win, 3 + patch_idx * 3 + 1, "%8d", patch->originx);
