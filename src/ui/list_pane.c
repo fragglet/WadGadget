88460dd92e85a57e8eb53f59df10401bd9e68de7
Simon Howard <fraggle@soulsphere.org>
2025-04-15 15:54:59 -0400

    Initialize list pane fields to sane values

--- src/ui/list_pane.c
+++ src/ui/list_pane.c
@@ -220,6 +220,8 @@ void UI_ListPaneInit(struct list_pane *p, WINDOW *w,
 	p->funcs = funcs;
 	p->data = data;
 	p->title = NULL;
+	p->selected = 0;
+	p->window_offset = 0;
 }
 
 int UI_ListPaneSelected(struct list_pane *p)
