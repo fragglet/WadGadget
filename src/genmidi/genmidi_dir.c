b5b46510da0b98079599ae77ce467c504f74ae91
Simon Howard <fraggle@soulsphere.org>
2025-05-02 00:22:47 -0400

    Convert genmidi_dir to use new lump_dir superclass

--- src/genmidi/genmidi_dir.c
+++ src/genmidi/genmidi_dir.c
@@ -12,6 +12,7 @@
 #include <stdlib.h>
 
 #include "common.h"
+#include "fs/lump_dir.h"
 #include "fs/vfile.h"
 #include "fs/vfs.h"
 #include "genmidi/genmidi.h"
@@ -20,11 +21,8 @@
 #include "ui/title_bar.h"
 
 struct genmidi_dir {
-	struct directory dir;
+	struct lump_based_dir dir;
 	struct genmidi_bank bank;
-	struct directory *parent_dir;
-	uint64_t lump_serial;
-	int last_commit;
 };
 
 const struct file_type file_type_genmidi_bank = {"GENMIDI bank"};
@@ -89,8 +87,8 @@ static struct directory *GenmidiOpenDir(void *_dir,
 	struct genmidi_dir *dir = _dir;
 
 	if (entry == VFS_PARENT_DIRECTORY) {
-		VFS_DirectoryRef(dir->parent_dir);
-		return dir->parent_dir;
+		VFS_DirectoryRef(dir->dir.parent_dir);
+		return dir->dir.parent_dir;
 	}
 
 	return NULL;
@@ -128,123 +126,68 @@ static bool GenmidiDirRename(void *_dir, struct directory_entry *entry,
 	return true;
 }
 
-static void SaveBank(struct genmidi_dir *dir)
-{
-	struct wad_file *wf;
-	struct directory_entry *ent;
-	VFILE *out;
-
-	if (dir->last_commit == 0) {
-		return;
-	}
-
-	ent = VFS_EntryBySerial(dir->parent_dir, dir->lump_serial);
-	if (ent == NULL) {
-		return;
-	}
-
-	wf = VFS_WadFile(dir->parent_dir);
-	out = W_OpenLumpRewrite(wf, ent - dir->parent_dir->entries);
-	assert(GENMIDI_SaveBank(&dir->bank, out));
-	vfclose(out);
-
-	VFS_CommitChanges(dir->parent_dir, "update of GENMIDI bank");
-	UI_ShowNotice("%s lump updated.", ent->name);
-}
-
-static void GenmidiDirFree(void *_dir)
-{
-	struct genmidi_dir *dir = _dir;
-
-	SaveBank(dir);
-	VFS_DirectoryUnref(dir->parent_dir);
-}
-
-static bool GenmidiDirNeedCommit(void *_dir)
-{
-	struct genmidi_dir *dir = _dir;
-
-	return dir->bank.modified_count > dir->last_commit;
-}
-
-static void GenmidiDirCommit(void *_dir)
-{
-	struct genmidi_dir *dir = _dir;
-
-	dir->last_commit = dir->bank.modified_count;
-}
-
-static VFILE *GenmidiDirSaveSnapshot(void *_dir)
+static VFILE *GenmidiDirMarshal(void *_dir)
 {
 	struct genmidi_dir *dir = _dir;
 	VFILE *result = vfopenmem(NULL, 0);
 
-	assert(vfwrite(&dir->bank, sizeof(struct genmidi_bank), 1, result) ==
-	       1);
+	assert(GENMIDI_SaveBank(&dir->bank, result));
 	vfseek(result, 0, SEEK_SET);
 	return result;
 }
 
-static void GenmidiDirRestoreSnapshot(void *_dir, VFILE *in)
+static bool GenmidiDirUnmarshal(void *_dir, VFILE *in, int mod_count)
 {
 	struct genmidi_dir *dir = _dir;
-	assert(vfread(&dir->bank, sizeof(struct genmidi_bank), 1, in) == 1);
+	bool success;
+	dir->bank.modified_count = mod_count;
+	success = GENMIDI_LoadBank(&dir->bank, in);
 	vfclose(in);
-	dir->last_commit = dir->bank.modified_count;
+
+	return success;
 }
 
+static int GenmidiDirModifiedCount(void *_dir)
+{
+	struct genmidi_dir *dir = _dir;
+	return dir->bank.modified_count;
+}
+
+static const struct lump_based_dir_funcs lump_dir_funcs = {
+    GenmidiDirMarshal,
+    GenmidiDirUnmarshal,
+    GenmidiDirModifiedCount,
+};
+
 static const struct directory_funcs genmidi_dir_funcs = {
-    "Voice",                   // singular
-    "Voices",                  // plural
-    true,                      // ordered
-    GenmidiDirRefresh,         // refresh
-    NULL,                      // open
-    GenmidiOpenDir,            // open_dir
-    GenmidiDirRemove,          // remove
-    GenmidiDirRename,          // rename
-    GenmidiDirNeedCommit,      // need_commit
-    GenmidiDirCommit,          // commit
-    NULL,                      // swap_entries
-    GenmidiDirSaveSnapshot,    // save_snapshot
-    GenmidiDirRestoreSnapshot, // restore_snapshot
-    GenmidiDirFree,            // free
+    "Voice",                    // singular
+    "Voices",                   // plural
+    true,                       // ordered
+    GenmidiDirRefresh,          // refresh
+    NULL,                       // open
+    GenmidiOpenDir,             // open_dir
+    GenmidiDirRemove,           // remove
+    GenmidiDirRename,           // rename
+    VFS_LumpDirNeedCommit,      // need_commit
+    VFS_LumpDirCommit,          // commit
+    NULL,                       // swap_entries
+    VFS_LumpDirSaveSnapshot,    // save_snapshot
+    VFS_LumpDirRestoreSnapshot, // restore_snapshot
+    VFS_LumpDirFree,            // free
 };
 
 struct directory *GENMIDI_OpenDir(struct directory *parent,
                                   struct directory_entry *ent)
 {
 	struct genmidi_dir *dir = checked_calloc(1, sizeof(struct genmidi_dir));
-	struct directory_revision *rev;
-	VFILE *lump;
-	bool loaded;
 
-	lump = VFS_OpenByEntry(parent, ent);
-	loaded = GENMIDI_LoadBank(&dir->bank, lump);
-	vfclose(lump);
-	if (!loaded) {
+	dir->dir.dir.type = &file_type_genmidi_bank;
+	dir->dir.dir.directory_funcs = &genmidi_dir_funcs;
+
+	if (!VFS_LumpDirInit(&dir->dir, &lump_dir_funcs, parent, ent)) {
 		free(dir);
 		return NULL;
 	}
 
-	dir->dir.directory_funcs = &genmidi_dir_funcs;
-	dir->dir.type = &file_type_genmidi_bank;
-	dir->dir.path =
-	    StringJoin(DIR_SEPARATOR_S, parent->path, ent->name, NULL);
-	dir->dir.entries = NULL;
-	dir->dir.num_entries = 0;
-	dir->dir.readonly = parent->readonly;
-	dir->dir.parent_name =
-	    StringJoin("", "Back to ", PathBaseName(parent->path), NULL);
-
-	dir->last_commit = 0;
-	dir->parent_dir = parent;
-	dir->lump_serial = ent->serial_no;
-	VFS_DirectoryRef(dir->parent_dir);
-	VFS_DirectoryRef(&dir->dir);
-	VFS_Refresh(&dir->dir);
-
-	rev = VFS_SaveRevision(&dir->dir);
-	snprintf(rev->descr, VFS_REVISION_DESCR_LEN, "Initial version");
-
-	return &dir->dir;
+	return &dir->dir.dir;
 }
