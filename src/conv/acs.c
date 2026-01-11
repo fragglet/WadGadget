0dcf7a1e1db7e1d2f28891d0bf03889a79618610
Simon Howard <fraggle@soulsphere.org>
2026-01-11 00:00:06 -0500

    Add copyright notice

--- src/conv/acs.c
+++ src/conv/acs.c
@@ -1,3 +1,12 @@
+//
+// Copyright(C) 2026 Simon Howard
+//
+// You can redistribute and/or modify this program under the terms of
+// the GNU General Public License version 2 as published by the Free
+// Software Foundation, or any later version. This program is
+// distributed WITHOUT ANY WARRANTY; without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//
 
 #include <ctype.h>
 #include <stdarg.h>
